23ffcd3dd6b4a2e8459617d093a5fb64e002abca
lkunlong <lkunlong@mie.utoronto.ca>
2020-12-28 19:47:09 -0500

    change test

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
 # initialize package used
 from pyspark.sql import SparkSession
+from pyspark import SparkContext
 
 # main starting point
 if __name__ == '__main__':
-    spark = SparkSession.builder.appName("AI-Shop-Algorithm").getOrCreate()
+#    spark = SparkSession.builder.appName("AI-Shop-Algorithm").getOrCreate()
+    sc = SparkContext("local", "")
     print("start from here")
